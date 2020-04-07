0d85656c81ab6f30e9f7de44270df32ee650f271
AreebSiddiqui <areeb40@hotmail.com>
2020-04-08 02:49:11 +0500

    initial commit

---
+++ learningStore/index.js
@@ -0,0 +1,18 @@
+//createStore() is a factory function 
+function createStore() {
+    //1. The State Tree.
+    //2. Get the state.
+    //3. listen for the changes in state and responding back.
+    //4. Update the state.
+
+    //1.
+    const state
+    
+    //2.
+    const getState = () => state
+
+    return {
+        getState
+    }
+
+}
