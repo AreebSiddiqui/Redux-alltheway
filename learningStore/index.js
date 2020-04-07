064ec90fb8a52ee2032541ab97856658bf39a11e
AreebSiddiqui <areeb40@hotmail.com>
2020-04-08 03:21:58 +0500

    Topic: 3. listen for the changes in state and responding back. Message: Add the code responsible for listening and responding back to the changes in state torugh the store object

--- learningStore/index.js
+++ learningStore/index.js
@@ -1,18 +1,38 @@
 //createStore() is a factory function 
 function createStore() {
-    //1. The State Tree.
-    //2. Get the state.
-    //3. listen for the changes in state and responding back.
-    //4. Update the state.
+    // 1. The State Tree.
+    // 2. Get the state.
+    // 3. listen for the changes in state and responding back.
+    // 4. Update the state.
 
     //1.
     const state
     
+    //array which stores all the call back functions passed in subscribe method when its invoked by the object 'store'.
+    const listeners  = []
+
     //2.
     const getState = () => state
 
+    //pushing the callbacks in an array.
+    const subscribe = (listener) => {
+        listeners.push(listener)
+    }
+
     return {
-        getState
+        getState,
+        subscribe
     }
 
 }
+// creating an object name 'store' 
+const store = createStore()
+// invoking subscribe method on the object and passing a call back function
+store.subscribe(()=> {
+    console.log("The new state is", store.getState)
+})
+
+// invoking subscribe method on the object and passing a call back function
+store.subscribe(()=> {
+    console.log("The state has been changed")
+})
